165372071b854c493e95a3f448340842f3668e83
Cynthia Greenblatt <cynthiagreenblatt@Cynthias-MacBook-Air.local>
2019-04-13 16:59:40 -0700

    feat: Add dispatch to props function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { handleInitialData } from '../actions/shared';
@@ -16,21 +16,39 @@ import '../App.css';
 library.add(fas);
 
 class App extends Component {
+
   componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(handleInitialData());
+    this.props.handleInitialData();
+  }
+
+  renderLeaderBoard = () => {
+    const { authedUser } = this.props;
+
+    return authedUser
+      ? <LeaderBoard />
+      : <Redirect to={{pathname: '/', state: { referrer: '/leaderboard' },}} />;
+  }
+
+  renderNewPoll = ()  => {
+    const { authedUser } = this.props;
+
+    return authedUser
+      ? <NewPoll />
+      : <Redirect to={{pathname: '/', state: { referrer: '/add' },}} />;
   }
 
   render() {
+    const { authedUser } = this.props;
     return (
       <Router>
         <div className="App">
-          <AuthedUser />
+          {authedUser && <AuthedUser />}
           <Switch>
             <Route path="/" exact component={SignIn} />
-            <Route path="/polls/:status" exact component={Polls} />
-            <Route path="/add" exact component={NewPoll} />
-            <Route path="/leaderboard" exact component={LeaderBoard} />
+            <Route path="/polls/:status" component={Polls} />
+            <Route path="/polls" exact component={Polls} />
+            <Route path="/add" exact render={this.renderNewPoll} />
+            <Route path="/leaderboard" exact render={this.renderLeaderBoard} />
             <Route path="/poll/:questionId" exact component={Poll} />
             <Route component={PageNotFound} />
           </Switch>
@@ -40,4 +58,13 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+
+function mapStateToProps({ authedUser }) {
+  return { authedUser };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { handleInitialData : () => dispatch(handleInitialData()) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
